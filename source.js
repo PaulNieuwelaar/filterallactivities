5f267c3f70a552a809112b8b2c505ef5652b6b9a
Paul Nieuwelaar <paulz12@live.com>
2017-09-01 16:05:13 +1200

    Create source.js

---
+++ source.js
@@ -0,0 +1,26 @@
+function filterAllActivities() {
+    $("#navActivities").click(function () {
+        _filterAllActivitiesBase();
+    });
+}
+
+function _filterAllActivitiesBase() {
+    var frame = $("#areaActivitiesFrame");
+    if (frame == null || frame.length == 0) {
+        setTimeout(_filterAllActivitiesBase, 100);
+        return;
+    }
+
+    frame.on("load", function () {
+        var entityName = Xrm.Page.data.entity.getEntityName();
+        var entity = entityName.charAt(0).toUpperCase() + entityName.substr(1);
+        var doc = this.contentWindow.document;
+        var filterOn = doc.getElementById("crmGrid_" + entity + "_ActivityPointers_datefilter");
+
+        filterOn.value = "All";
+
+        var evt = doc.createEvent("HTMLEvents");
+        evt.initEvent("change", false, true);
+        filterOn.dispatchEvent(evt);
+    });
+}
